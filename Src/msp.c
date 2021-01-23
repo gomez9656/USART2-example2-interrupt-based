0ca40ca41266df346cc4ad24d7dba27ab048e24c
juanpg9656@gmail.com <juanpg9656@gmail.com>
2021-01-23 17:56:12 -0500

    Added low level initialization for USART2

--- Src/msp.c
+++ Src/msp.c
@@ -22,3 +22,31 @@ void HAL_MspInit(void)
 	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
 	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
 }
+
+/*
+ * Configure the low level initialization of the UART2 peripheral
+ */
+void HAL_UART_MspInit(UART_HandleTypeDef *huart){
+
+	GPIO_InitTypeDef gpio_uart;
+
+	//1. Enable the clock
+	__HAL_RCC_USART2_CLK_ENABLE();
+
+	//2. Pin muxing configuration
+	gpio_uart.Pin = GPIO_PIN_2;
+	gpio_uart.Mode = GPIO_MODE_AF_PP;
+	gpio_uart.Pull = GPIO_PULLUP;
+	gpio_uart.Speed = GPIO_SPEED_FREQ_LOW;
+	gpio_uart.Alternate = GPIO_AF7_USART2; //USART2_TX
+
+	HAL_GPIO_Init(GPIOA, &gpio_uart); //Initialize GPIOA2 with gpio_uart configuration
+
+	gpio_uart.Pin = GPIO_PIN_3;	//USART2_RX
+	HAL_GPIO_Init(GPIOA, &gpio_uart); //Initialize GPIOA3 with gpio_uart configuration
+
+	//3. Enable the IRQ in the NVIC and set up priority
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+	HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
+
+}
